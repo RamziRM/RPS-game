18f2a4a0f753e7de28c76df4fed2e332a914dda7
Ramzi <115042042+RamziRM@users.noreply.github.com>
2023-01-07 02:27:39 -0300

    fix loop bug - refactor game, scoring and selection

--- app.js
+++ app.js
@@ -1,65 +1,54 @@
-
-
-function getComputerChoice() {
-    let number =(Math.random() * 3);
-    if (number <= 1) {
-        return 'rock';
-    }
-    else if (number >= 2) {
-        return 'paper';
-    }
-    else return 'scissors';
-}
-//console.log(getComputerChoice());
+// Initialize variablesz;
+const choices = ['rock', 'paper', 'scissors'];
 
 let playerScore = 0;
 let computerScore = 0;
 
-function playRound(playerSelection, computerSelection) {
-    if (playerSelection === computerSelection) {
-        return `It's a tie! You have both selected ${playerSelection}. Try again!`
-    } else if (playerSelection == 'rock' && computerSelection == 'paper') {
-        computerScore++;
-        return `You lose! Current score: ${playerScore} to ${computerScore}`
-    } else if (playerSelection == 'rock' && computerSelection == 'scissors') {
-        playerScore++;
-        return `You win! Current score: ${playerScore} to ${computerScore}`
-    } else if (playerSelection == 'paper' && computerSelection == 'scissors') {
-        computerScore++;
-        return `You lose! Current score: ${playerScore} to ${computerScore}`
-    } else if (playerSelection == 'paper' && computerSelection == 'rock') {
-        playerScore++;
-        return `You win! Current score: ${playerScore} to ${computerScore}`
-    } else if (playerSelection == 'scissors' && computerSelection == 'paper') {
-        computerScore++;
-        return `You lose! Current score: ${playerScore} to ${computerScore}`
-    } else if (playerSelection == 'scissors' && computerSelection == 'rock') {
-        computerScore++;
-        return `You lose! Current score: ${playerScore} to ${computerScore}` 
+let computerSelection = choices[Math.floor(Math.random() * 3)];
+//console.log(Math.floor(Math.random() * 3));
+
+// Apply loop to 5 points - higher level - Prompt
+while (playerScore < 5 && computerScore < 5) {
+    let playerSelection = prompt('Type Rock, Paper or Scissors: ');
+
+    // Check if input is a valid choice
+    if (choices.indexOf(playerSelection) === -1) {
+        alert('Invalid input. Please try again, choose one of listed options.');
     }
+
+
+    function playRound(playerSelection, computerSelection) {
+        if (playerSelection === computerSelection) {
+            return `It's a tie! You have both selected ${playerSelection}. Try again!`
+        } else if (playerSelection == 'rock' && computerSelection == 'paper') {
+            computerScore++;
+            return `You lose! Current score: ${playerScore} to ${computerScore}`
+        } 
+        
+        else if (playerSelection == 'rock' && computerSelection == 'scissors'   
+            ||
+            playerSelection == 'paper' && computerSelection == 'rock'
+            ||
+            playerSelection == 'scissors' && computerSelection == 'paper'
+            ) {
+            playerScore++;
+            return `You win! Current score: ${playerScore} to ${computerScore}`;
+        } 
+        
+        // Lost rounds == rest of possible outcomes - Refactor, eliminated several lines
+        else {
+            computerScore++;
+            return `You lose! Current score: ${playerScore} to ${computerScore}`
+        }
+    }
+    
 }
 
-const computerSelection = getComputerChoice();
-//const playerSelection =
-
-//console.log(playRound(playerSelection, computerSelection));
-
-
-function game() {
-    for (let i = 0; i < 5; i++) {
-        let playerSelection = prompt('Type Rock, Paper or Scissors: ')
-        playerSelection = playerSelection.toLowerCase();
-        const computerSelection = getComputerChoice()
-        console.log(playRound(playerSelection, computerSelection))
-    }
-    // Final score after loop - Display winner/loser
-    if (playerScore > computerScore) {
-        return `WINNER! You have defeated the computer.
-        Final score: ${playerScore} to ${computerScore}`
-    } else if (playerScore < computerScore) {
-        return `You lost. AGI is superior. 
-        Final score: ${playerScore} to ${computerScore}`
-    }
-}
-
-game();
+    // Gameover 
+    if (playerScore === 5) {
+        alert(`WINNER! You have defeated the computer.
+        Final score: ${playerScore} to ${computerScore}`)
+    } else {
+        alert(`You lost. AGI is superior. 
+        Final score: ${playerScore} to ${computerScore}`)
+    }    
